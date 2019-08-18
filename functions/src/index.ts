bb4c1e6e05bec085e6fcc78e57d7db4e257ecc5f
mikan3rd <mikan.no.kikoushi@gmail.com>
2019-08-18 19:06:10 +0900

    chore: testFunctionの追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
 
 export const helloWorld = functions.region('asia-northeast1').https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
+});
+
+export const testFunction = functions.region('asia-northeast1').https.onCall((data, context: any) => {
+  const uid = context.auth.uid;
+  const name = context.auth.token.name || null;
+  const email = context.auth.token.email || null;
+  return { uid, name, email };
 });

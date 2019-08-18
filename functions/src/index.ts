43d278709071f7a5c97700205b7a14f61b026c11
mikan3rd <mikan.no.kikoushi@gmail.com>
2019-08-18 22:45:58 +0900

    feat: updateTwitterUserの実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -24,8 +24,31 @@ export const updateGitHubUser = functions.region('asia-northeast1').https.onCall
     .doc(uid)
     .set(userData, { merge: true })
     .catch(err => {
-      throw new functions.https.HttpsError('internal', 'Failed set user', err);
+      throw new functions.https.HttpsError('internal', 'Failed to set user', err);
     });
 
   return { uid, github_access_token, github_user_name };
+});
+
+export const updateTwitterUser = functions.region('asia-northeast1').https.onCall((data: any, context: any) => {
+  const { twitter_access_token, twitter_access_token_secret, twitter_screen_name } = data;
+  const { uid } = context.auth;
+
+  const userCollection = db.collection('users');
+  const userData = {
+    uid,
+    twitter_access_token,
+    twitter_access_token_secret,
+    twitter_screen_name,
+    update_at: admin.firestore.FieldValue.serverTimestamp(),
+  };
+
+  userCollection
+    .doc(uid)
+    .set(userData, { merge: true })
+    .catch(err => {
+      throw new functions.https.HttpsError('internal', 'Failed to set user', err);
+    });
+
+  return { uid, twitter_access_token, twitter_access_token_secret, twitter_screen_name };
 });
